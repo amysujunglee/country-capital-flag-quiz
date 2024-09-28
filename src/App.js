10028aae91dc61007d2ec88619955db835b3bd68
Amy Lee <amy.sujunglee@gmail.com>
2024-09-27 20:00:24 -0700

    add displayData function

--- src/App.js
+++ src/App.js
@@ -6,7 +6,19 @@ function App() {
     const url = 'https://countriesnow.space/api/v0.1/countries/capital'
     fetch(url)
       .then(res => res.json())
-      .then(data => data)
+      .then(data => { displayData(data.data[0]); })
+  }
+
+  const displayData = (country) => {
+    if (!country) <div></div>
+    console.log('display data', country)
+    return (
+      <>
+        <p>Country Name: {country?.name}</p>
+        <p>Capital:</p>
+        <p>Flag:</p>
+      </>
+    )
   }
 
   fetchData()
@@ -14,9 +26,7 @@ function App() {
   return (
     <main className="">
       <h1>Country / Capital / Flag Quiz</h1>
-      <p>Country Name:</p>
-      <p>Capital:</p>
-      <p>Flag:</p>
+      {displayData()}
     </main>
   );
 }
